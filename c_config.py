536b327d7f65adc496d895af2b6488692788b4ab
Gustavo Rabino <79431601+Neogus@users.noreply.github.com>
2023-10-10 01:19:40 +0200

    Update c_config.py

--- c_config.py
+++ c_config.py
@@ -1,3 +1,4 @@
-B_API_KEY = 'Fill with Binance API Key'
-B_API_SECRETKEY = 'Fill with Binance API Secret Key'
-T_API_KEY = 'Fill with Telegram Bot API Key'
+B_API_KEY = 'Fill with Binance API Key'  # This has a string format
+B_API_SECRETKEY = 'Fill with Binance API Secret Key'  # This has a string format
+T_API_KEY = 'Fill with Telegram Bot API Key'  # This has a string format
+chat_id = 'Fill with Telegram chat ID number'  # This has a number format
